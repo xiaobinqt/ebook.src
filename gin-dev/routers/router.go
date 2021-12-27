b3a3ac1dfdf0acc8a477b9b702b776ac5ca2ac3c
weibin <xiaobinqt@163.com>
2021-12-27 18:04:01 +0800

    gin-dev

--- gin-dev/routers/router.go
+++ gin-dev/routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"gin-dev/middleware/jwt"
 	"gin-dev/pkg/setting"
+	"gin-dev/routers/api"
 	v1 "gin-dev/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
@@ -15,7 +17,10 @@ func InitRouter() *gin.Engine {
 
 	gin.SetMode(setting.RunMode)
 
+	r.GET("/auth", api.GetAuth)
+
 	apiv1 := r.Group("/api/v1")
+	apiv1.Use(jwt.JWT())
 	{
 		//获取标签列表
 		apiv1.GET("/tags", v1.GetTags)

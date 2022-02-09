fec546a665de2a6649518bb06f5f08f250bf5f10
weibin <xiaobinqt@163.com>
2022-02-09 14:52:59 +0800

    go.src

---
+++ dhsjjg/chapter4/链队列.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+type QNode struct {
+	Elem int
+	Next *QNode
+}
+
+func main() {
+	fmt.Println(11)
+}
---
+++ gee-7days/high-performance-go/go.mod
@@ -0,0 +1,5 @@
+module go.src/gee-7days/high-performance-go
+
+go 1.16
+
+require github.com/pkg/profile v1.6.0
